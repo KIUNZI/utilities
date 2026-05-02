d35e044c45bf7f2fb843c156870968e6ccf99fad
jamarsto <jason@jasonmarston.me.uk>
2026-05-02 14:26:16 +0100

    Test of branch protection rules.

--- utility-exception-mapper/build.gradle.kts
+++ utility-exception-mapper/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("uk.co.jasonmarston.project.standards.quarkus-library")
 }
 
+// Used in github pages
 description = "Exception-to-response mapping utilities for REST services."
 
 dependencies {
